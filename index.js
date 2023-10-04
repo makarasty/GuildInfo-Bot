80092029dd41ffa66c339c6ba41fcb417b2e6a2e
MaKarastY <71918286+makarasty@users.noreply.github.com>
2023-10-04 18:22:00 +0300

    🚀 Added small performance improvements
    The bluebird library was used to improve performance, and other decisions were made to improve performance and improve the interface

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { Client, Events, ActivityType, ChannelType } = require("discord.js");
 const Config = require("./config.js");
 
 const Promise = require('bluebird');
-
 global.Promise = Promise;
 
 class DiscordBot extends Client {
@@ -16,21 +15,20 @@ class DiscordBot extends Client {
 		})
 	}
 	async Init() {
-		this.Events();
-
+		await this.Events();
 		await this.login(Config.Token);
-		console.info(`Discord Bot started! (${this.user.tag})`);
+		console.info(`Discord Bot started! (${this.user.tag})\n`);
 	}
 	async Events() {
 		async function BotReady(client) {
 
 			// ? Setting bot status
-			setTimeout(() => SetBotStatus(client), 100_000);
 			SetBotStatus(client);
+			setTimeout(() => SetBotStatus(client), 100_000);
 
 			// ? Starting server-info system
-			setInterval(() => ServerInfoSystem(client), 401_000);
 			ServerInfoSystem(client);
+			setInterval(() => ServerInfoSystem(client), 401_000);
 		}
 
 		this.on(Events.ClientReady, BotReady);
@@ -45,10 +43,17 @@ class DiscordBot extends Client {
  * @returns 
  */
 async function FormatChannelName(text, client, guild) {
-	const guildChannels = await guild.channels.fetch();
-	const guildMembers = await guild.members.fetch({ withPresences: true });
-	const guildEmojis = await guild.emojis.fetch();
-	const guildRoles = await guild.roles.fetch();
+	const [
+		guildMembers,
+		guildChannels,
+		guildEmojis,
+		guildRoles
+	] = await Promise.all([
+		guild.members.fetch({ withPresences: true }),
+		guild.channels.fetch(),
+		guild.emojis.fetch(),
+		guild.roles.fetch()
+	])
 
 	const botUsers = guildMembers.filter(({ user }) => user.bot);
 	const users = guildMembers.filter(({ user }) => !user.bot);
@@ -56,11 +61,21 @@ async function FormatChannelName(text, client, guild) {
 	const onlineStatusArray = ['online', 'idle', 'dnd'];
 	const offlineStatusArray = ['offline', 'invisible'];
 
-	const onlineUsersCount = users.filter(({ presence }) => presence ? onlineStatusArray.includes(presence.status) : undefined).size;
-	const usersStatusOffline = users.filter(({ presence }) => presence ? offlineStatusArray.includes(presence.status) : undefined).size;
-	const usersStatusOnline = users.filter(({ presence }) => presence ? presence.status === 'online' : undefined).size;
-	const usersStatusIdle = users.filter(({ presence }) => presence ? presence.status === 'idle' : undefined).size;
-	const usersStatusDnd = users.filter(({ presence }) => presence ? presence.status === 'dnd' : undefined).size;
+	const onlineUsersCount = users.filter(({ presence }) =>
+		presence ? onlineStatusArray.includes(presence.status) : undefined
+	).size
+	const usersStatusOffline = users.filter(({ presence }) =>
+		presence ? offlineStatusArray.includes(presence.status) : undefined
+	).size
+	const usersStatusOnline = users.filter(({ presence }) =>
+		presence ? presence.status === 'online' : undefined
+	).size
+	const usersStatusIdle = users.filter(({ presence }) =>
+		presence ? presence.status === 'idle' : undefined
+	).size
+	const usersStatusDnd = users.filter(({ presence }) =>
+		presence ? presence.status === 'dnd' : undefined
+	).size
 
 	const animetedEmojis = guildEmojis.filter(({ animated }) => animated);
 	const staticEmojis = guildEmojis.filter(({ animated }) => !animated);
@@ -101,7 +116,7 @@ async function FormatChannelName(text, client, guild) {
 
 	const newText = text.replace(regexp, (word) => {
 		return globalModificatorsObject[word.toLowerCase()] ?? word
-	});
+	})
 
 	return newText
 }
@@ -110,41 +125,44 @@ async function FormatChannelName(text, client, guild) {
  * @param {DiscordBot} client 
  */
 async function ServerInfoSystem(client) {
-	for (const guildConfig of Config.ServerInfo.Guilds) {
+	await Promise.map(Config.ServerInfo.Guilds, async (guildConfig) => {
 		const { channels, guildId } = guildConfig;
 
 		const guild = await client.guilds.fetch(guildId)
-			.catch(() => console.error(`Guild ${guildId} not found!`));
+			.catch(() => console.error(`Guild "${guildId}": Guild not found! Check config!`));
 
-		if (!guild) continue;
+		if (guild) {
+			await Promise.map(channels, async (channelConfig) => {
+				const { name, id } = channelConfig;
 
-		for (const channelConfig of channels) {
-			const { name, id } = channelConfig;
+				const channel = await guild.channels.fetch(id)
+					.catch(() => console.error(`Guild "${guild.name}": Channel "${id}" not found!`));
 
-			const channel = await guild.channels.fetch(id)
-				.catch(() => console.error(`Channel ${id} not found!`));
+				if (channel) {
+					const newChannelName = await FormatChannelName(name, client, guild);
+					const lastChannelName = channel.name;
 
-			if (!channel) continue;
+					if (channel.name !== newChannelName) {
+						console.info(`Guild "${guild.name}": Waiting for renaming: "${newChannelName}".`);
 
-			const newChannelName = await FormatChannelName(name, client, guild);
-
-			if (channel.name != newChannelName) {
-				const lastChannelName = channel.name;
-				console.info(`Waiting for renaming: ${newChannelName}`);
-
-				channel.setName(newChannelName)
-					.then(() => console.info(`Channel renamed: ${lastChannelName} => ${newChannelName}`))
-					.catch(() => console.error(`Channel not renamed: ${lastChannelName} !=> ${newChannelName}`));
-			}
+						channel
+							.setName(newChannelName)
+							.then(() => console.info(`Guild "${guild.name}": Channel renamed successfully: "${lastChannelName}" => "${newChannelName}".`))
+							.catch(() => console.error(`Guild "${guild.name}": Channel not renamed: "${lastChannelName}" !=> ${newChannelName}".`));
+					} else {
+						console.info(`Guild "${guild.name}": Channel renaming skipped: "${lastChannelName}" === "${newChannelName}".`);
+					}
+				}
+			})
 		}
-	}
+	})
 }
 /**
  * 
  * @param {DiscordBot} param0 
  */
 async function SetBotStatus({ user }) {
-	user.setActivity('Example Server-Info Bot',
+	user.setActivity('Example Server-Info made by https://github.com/makarasty',
 		{
 			type: ActivityType.Playing,
 			url: 'https://www.twitch.tv/makarasty'
